1014919e3b31c768cd9b086b50c575c50921c781
Vivek <ojhavivek24@gmail.com>
2025-06-05 14:53:29 +0530

    extended properties

--- client/src/components/common/InputComponent.tsx
+++ client/src/components/common/InputComponent.tsx
@@ -4,18 +4,21 @@ import type { IconType } from "react-icons";
 interface InputProps {
   title: string;
   placeholder: string;
-  inputTagStyle?: string;
-  inputType?:string;
+  inputType?: string;
   Icon:IconType;
-  iconStyle?:string;
+  iconSize?: string;
+  inputTagStyle?: string;
+  titleStyle?: string;
+  componentStyle?: string;
+  iconColor?: string;
 }
 
-const InputComponent = ({ title, placeholder, inputTagStyle, inputType="text",Icon,iconStyle}: InputProps) => {
+const InputComponent = ({ title, placeholder, inputTagStyle, inputType="text",Icon,iconSize,titleStyle, componentStyle, iconColor}: InputProps) => {
   return (
-    <div className="flex flex-col gap-1 text-white">
+    <div className={cn(`flex flex-col gap-1 text-white ${componentStyle}`)}>
       <div className="flex items-center gap-2 ">
-        <Icon className={cn(`h-6 w-6 ${iconStyle}`)}/>
-        <span className="text-lg tracking-wide font-mono">{title}</span>
+        <span className={cn(`${iconColor}`)}><Icon className={cn(`h-6 w-6 ${iconSize}`)}/></span>
+        <span className={cn(`text-lg tracking-wide font-mono ${titleStyle}`)}>{title}</span>
       </div>
       <div>
         <input
