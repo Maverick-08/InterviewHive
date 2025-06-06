4c62f08cc8e456abdfe311432510b5db9cc6a808
Vivek <ojhavivek24@gmail.com>
2025-06-06 05:56:06 +0530

    extended button property

--- client/src/components/common/WhiteButton.tsx
+++ client/src/components/common/WhiteButton.tsx
@@ -1,18 +1,40 @@
-import { cn } from "@/lib/utils"
-import type { ButtonHTMLAttributes } from "react"
+import { cn } from "@/lib/utils";
+import type { ButtonHTMLAttributes } from "react";
+import type { IconType } from "react-icons";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-    text:string,
-    className?:string
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  className?: string;
+  Icon?: IconType;
+  iconSize?: string;
+  containerStyle?: string;
 }
 
-
-const WhiteButton = ({text,className,...rest}:ButtonProps) => {
+const WhiteButton = ({
+  text,
+  className,
+  Icon,
+  iconSize,
+  containerStyle,
+  ...rest
+}: ButtonProps) => {
   return (
-    <button {...rest} className={cn(` px-4 py-1 rounded-md bg-white hover:bg-white/80 text-black text-lg cursor-pointer ${className}`)}>
-      {text}
+    <button
+      {...rest}
+      className={cn(
+        ` px-4 py-1 rounded-md bg-white hover:bg-white/80 text-black text-lg cursor-pointer font-mono ${className}`
+      )}
+    >
+      {Icon ? (
+        <div className={cn(`flex items-center gap-2 text-lg ${containerStyle}`)}>
+          <Icon className={cn(`h-6 w-6 ${iconSize}`)} />
+          <span>{text}</span>
+        </div>
+      ) : (
+        <span>{text}</span>
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default WhiteButton
+export default WhiteButton;
