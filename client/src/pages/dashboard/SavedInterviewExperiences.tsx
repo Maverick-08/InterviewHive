4deced95a645fb7c2c5a0f28f672628e2c172a6e
Vivek <ojhavivek24@gmail.com>
2025-06-08 13:44:46 +0530

    ui update in saved interview experience page

--- client/src/pages/dashboard/SavedInterviewExperiences.tsx
+++ client/src/pages/dashboard/SavedInterviewExperiences.tsx
@@ -16,16 +16,37 @@ const SavedInterviewExperiences = () => {
 
   if (isLoading) {
     return (
-      <div className="w-full h-[60vh] flex justify-center items-center">
+      <div className="w-full h-screen flex justify-center items-center">
         <Loading />
       </div>
     );
   }
 
   return (
-    <div className="w-full min-h-screen max-w-7xl pt-24  px-8 lg:px-20 text-white">
-      <ListExperiences interviewData={tempData}/>
-    </div>
+    <div className="w-full max-w-7xl pt-24 lg:pt-32">
+          <div className="flex flex-col gap-4 px-4 text-white font-mono">
+            {/* Top title and search component  */}
+            <div className="px-2 flex flex-col md:flex-row items-center gap-4 md:gap-0">
+              {/* Title component  */}
+              <div className="flex-4 bg-gradient-to-b from-white to-zinc-600 bg-clip-text text-transparent ">
+                <span className="text-4xl font-semibold">Saved Interviews</span>
+              </div>
+    
+              {/* search bar */}
+              <div className="flex-1 w-full flex items-center justify-between gap-4 ">
+                {/* search bar  */}
+                <input
+                  type="text"
+                  placeholder="Search Company Name"
+                  className="flex-1 focus:outline-none border rounded-sm border-neutral-500 px-2 py-2 placeholder:text-neutral-400"
+                />
+              </div>
+            </div>
+    
+            {/* Display experiences  */}
+            <ListExperiences interviewData={tempData} />
+          </div>
+        </div>
   );
 };
 
