e4587c0e92196bdc560c65cacba285a2f7f49c8c
Vivek <ojhavivek24@gmail.com>
2025-06-09 13:41:21 +0530

    added signin and signup button with google and github

--- client/src/pages/authentication/RegistrationComponent.tsx
+++ client/src/pages/authentication/RegistrationComponent.tsx
@@ -6,19 +6,25 @@ import { LuNotebookPen } from "react-icons/lu";
 import { FaRegCalendarCheck } from "react-icons/fa6";
 import WhiteButton from "@/components/common/WhiteButton";
 import { useNavigate } from "react-router-dom";
+import { AiOutlineChrome, AiOutlineGithub } from "react-icons/ai";
 
 const RegistrationComponent = () => {
   const navigate = useNavigate();
   return (
     <div className="w-full max-w-md px-4 flex flex-col gap-4 text-white select-none">
+
+      {/* Back Button  */}
       <span
         onClick={() => navigate("/")}
         className="absolute lg:hidden top-6 left-4 text-neutral-400 cursor-pointer"
       >
         <IoChevronBackOutline className="h-6 w-6" />
       </span>
+
+      {/* Top Heading - Welcome Back  */}
       <p className="text-2xl text-center">Welcome Back ! 👋</p>
       <div className="flex flex-col gap-6">
+        {/* Username  */}
         <InputComponent
           title="Username"
           Icon={IoPersonOutline}
@@ -26,12 +32,15 @@ const RegistrationComponent = () => {
           placeholder="Username"
         />
         <div className="flex justify-between items-center gap-2">
+          {/* Course  */}
           <InputComponent
             title="Course"
             Icon={LuNotebookPen}
             inputType={"text"}
             placeholder="MCA / CSE / MBA"
           />
+
+          {/* Year of passing out   */}
           <InputComponent
             title="Year"
             Icon={FaRegCalendarCheck}
@@ -39,12 +48,16 @@ const RegistrationComponent = () => {
             placeholder=""
           />
         </div>
+
+        {/* Email  */}
         <InputComponent
           title="Email"
           Icon={MdOutlineEmail}
           inputType={"email"}
           placeholder="name@example.com"
         />
+        
+        {/* Password  */}
         <InputComponent
           title="Password"
           Icon={IoKeyOutline}
@@ -52,8 +65,11 @@ const RegistrationComponent = () => {
           placeholder="your password"
         />
       </div>
+
+      {/* Register  */}
       <WhiteButton text="Register" className="mt-4 w-full font-mono" />
-      <div>
+
+      <div className="pb-4 border-b border-[#333333]">
         Have an account ?{" "}
         <span
           onClick={() => navigate("/login")}
@@ -61,6 +77,20 @@ const RegistrationComponent = () => {
         >
           Sign In
         </span>
+      </div>
+
+      {/* Sign in with google or github */}
+      <div className="flex flex-col gap-4">
+        <WhiteButton
+          text="Sign in with Google"
+          className="w-full font-mono flex items-center justify-center gap-2"
+          Icon={AiOutlineChrome}
+        />
+        <WhiteButton
+          text="Sign in with Github"
+          className="w-full font-mono flex items-center justify-center gap-2"
+          Icon={AiOutlineGithub}
+        />
       </div>
     </div>
   );
--- client/src/pages/authentication/SignupComponent.tsx
+++ client/src/pages/authentication/SignupComponent.tsx
@@ -1,6 +1,8 @@
 import InputComponent from "@/components/common/InputComponent";
 import { MdOutlineEmail } from "react-icons/md";
 import { IoChevronBackOutline, IoKeyOutline } from "react-icons/io5";
+import { AiOutlineChrome } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 import WhiteButton from "@/components/common/WhiteButton";
 import { useNavigate } from "react-router-dom";
 
@@ -8,21 +10,27 @@ const SignupComponent = () => {
   const navigate = useNavigate();
   return (
     <div className="relative w-full max-w-md px-4 flex flex-col justify-center items-center gap-2 text-white select-none">
+      {/* Back Button  */}
       <span
         onClick={() => navigate("/")}
         className="absolute lg:hidden top-6 left-4 text-neutral-400 cursor-pointer"
       >
         <IoChevronBackOutline className="h-6 w-6" />
       </span>
+
+      {/* Top Heading - Welcome Back */}
       <p className="text-2xl text-center">Welcome Back ! 👋</p>
       <div className="w-full">
         <div className="flex flex-col gap-6">
+          {/* Email Component  */}
           <InputComponent
             title="Email"
             Icon={MdOutlineEmail}
             inputType={"email"}
             placeholder="name@example.com"
           />
+
+          {/* Password Component  */}
           <InputComponent
             title="Password"
             Icon={IoKeyOutline}
@@ -30,15 +38,21 @@ const SignupComponent = () => {
             placeholder="your password"
           />
         </div>
+
+        {/* Reset Password  */}
         <p className="my-4 text-right underline cursor-pointer">
           Reset password
         </p>
+
+        {/* SignIn Button  */}
         <WhiteButton
           text="Sign In"
           onClick={() => navigate("/interview/dashboard")}
           className="w-full font-mono"
         />
-        <div className="my-4 ">
+
+        {/* SignUp  */}
+        <div className="my-4 pb-4 border-b border-[#333333]">
           Don't have an account ?{" "}
           <span
             onClick={() => navigate("/register")}
@@ -46,6 +60,20 @@ const SignupComponent = () => {
           >
             Sign Up
           </span>
+        </div>
+
+        {/* Sign in with google or github */}
+        <div className="flex flex-col gap-4">
+          <WhiteButton
+            text="Sign in with Google"
+            className="w-full font-mono flex items-center justify-center gap-2"
+            Icon={AiOutlineChrome}
+          />
+          <WhiteButton
+            text="Sign in with Github"
+            className="w-full font-mono flex items-center justify-center gap-2"
+            Icon={AiOutlineGithub}
+          />
         </div>
       </div>
     </div>
