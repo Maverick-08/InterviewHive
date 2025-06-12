838cd6eeef4d9b361ab0eae357e3b42aa7c74910
Vivek <ojhavivek24@gmail.com>
2025-06-12 14:44:19 +0530

    bug fix

--- server/src/config/app-config.ts
+++ server/src/config/app-config.ts
@@ -1,6 +1,6 @@
 export const origins = [
   "http://localhost:5173",
-  "https://interview-hive-five.vercel.app/",
+  "https://interview-hive-five.vercel.app",
 ];
 
 export const isCredentialsAllowed = true;
