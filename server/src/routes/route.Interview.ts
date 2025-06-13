0c314a75e05c44233fc2a6b7a59e7ff2ced2c12c
Vivek <ojhavivek24@gmail.com>
2025-06-13 16:25:24 +0530

    file name updates

--- server/src/routes/route.Interview.ts
+++ server/src/routes/route.Interview.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { getAllInterviewsController } from "../controllers/controller.interview.getAllInterviews";
 import { addInterviewsController } from "../controllers/controller.interview.addInterviewExperienxe";
-import { getFilteredInterviewsController } from "../controllers/contoller.interview.getFilteredInterviewsContoller";
-import { getUserInterviewsController } from "../controllers/contoller.interviews.getUserInterviews";
+import { getFilteredInterviewsController } from "../controllers/controller.interview.getFilteredInterviews";
+import { getUserInterviewsController } from "../controllers/controller.interviews.getUserInterviews";
 import { getInterviewByIdController } from "../controllers/controller.interview.getInterviewById";
 import { deleteInterviewByIdController } from "../controllers/controller.interview.deleteUserInterview";
 import { updateInterviewExperienceController } from "../controllers/controller.interview.updateInterviewExperience";
