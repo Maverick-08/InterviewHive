91300db8804e7e6e5c79b951e0ab6cbad6882550
Vivek <ojhavivek24@gmail.com>
2025-06-17 13:35:01 +0530

    deployment changes

--- server/src/config/app-config.ts
+++ server/src/config/app-config.ts
@@ -1,6 +1,7 @@
 export const origins = [
   "http://localhost:5173",
   "https://interview-hive-five.vercel.app",
+  "http://interview-hive.dev-projects.site"
 ];
 
 export const isCredentialsAllowed = true;
