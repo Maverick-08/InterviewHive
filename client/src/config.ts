000ddd167b6989a2d371d27ca991ca5e8403e9cb
Vivek <ojhavivek24@gmail.com>
2025-06-17 20:09:35 +0530

    config change

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://interview-hive.dev-projects.site"
--- server/src/config/app-config.ts
+++ server/src/config/app-config.ts
@@ -1,9 +1,8 @@
 export const origins = [
   "http://localhost:5173",
   "https://interview-hive-five.vercel.app",
+  "https://interview-hive.dev-projects.site",
   "http://interview-hive.dev-projects.site",
-  "http://localhost:80",
-  "http://localhost"
 ];
 
 export const isCredentialsAllowed = true;
