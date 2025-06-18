dd3289c1b2579f6431fcdb254dd838ef3e55aa88
Vivek <ojhavivek24@gmail.com>
2025-06-18 12:11:27 +0530

    added get ip code

--- server/src/config/app-config.ts
+++ server/src/config/app-config.ts
@@ -1,11 +1,28 @@
+import os from 'os'
+
+function getHostIPAddress() {
+  const interfaces = os.networkInterfaces();
+
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]!) {
+      // Skip over internal (i.e., 127.0.0.1) and non-IPv4 addresses
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+
+  return '127.0.0.1'; // fallback to localhost if no external address found
+}
+
+const ip = getHostIPAddress();
+
 export const origins = [
   "http://localhost:5173",
   "https://interview-hive-five.vercel.app",
   "https://interview-hive.dev-projects.site",
   "http://interview-hive.dev-projects.site",
-  "http://localhost",
-  "localhost",
-  "http://localhost:80"
+  `http://${ip}`
 ];
 
 export const isCredentialsAllowed = true;
