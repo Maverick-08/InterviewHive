d36e92834a6a9ddbf587e3f9e5353f4629dc5d9f
Vivek <ojhavivek24@gmail.com>
2025-06-18 14:09:21 +0530

    added localhost link

--- server/src/config/app-config.ts
+++ server/src/config/app-config.ts
@@ -1,5 +1,6 @@
 export const origins = [
   "http://localhost:5173",
+  "http://localhost",
   "https://interview-hive-five.vercel.app",
   "https://interview-hive.dev-projects.site",
   "http://interview-hive.dev-projects.site",
