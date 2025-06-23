bdeaa244aa9a62bc849e3834b0c59b5af3690be7
Vivek <ojhavivek24@gmail.com>
2025-06-23 08:03:06 +0530

    typo fix

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -4,7 +4,7 @@ import HeroSection from "./HeroSection";
 import Navbar from "./Navbar";
 import OrbitalSection from "./OrbitalSection";
 import Footer from "./Footer";
-import FaqSection from "./FaqSection";
+import FaqSection from "./FAQSection";
 const Landing = () => {
   return (
     <div className="w-full h-full bg-gradient-to-r from-[#161616] to-black">
