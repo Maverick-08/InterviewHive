a1d0f3cf069a1a37571f041889e0978f1db7830e
Vivek <ojhavivek24@gmail.com>
2025-06-26 11:51:01 +0530

    deleted migration

--- server/prisma/migrations/20250623112236_version_2_9/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `viewCount` on the `Interview` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "Interview" ADD COLUMN     "difficultyLevel" TEXT NOT NULL DEFAULT 'EASY',
-ALTER COLUMN "viewCount" SET DATA TYPE INTEGER;
