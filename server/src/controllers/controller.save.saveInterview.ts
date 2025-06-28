c8488ee36539c1a4767f5a71efc4fa319b2334e6
Vivek <ojhavivek24@gmail.com>
2025-06-28 16:06:40 +0530

    bug fix

--- server/src/controllers/controller.save.saveInterview.ts
+++ server/src/controllers/controller.save.saveInterview.ts
@@ -1,70 +1,66 @@
-import {Request,Response} from "express";
+import { Request, Response } from "express";
 import { code } from "../config/status-code";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const saveInterviewController = async (req:Request,res:Response) => {
-    try{
-        const payload = req.query as unknown as {interviewId:string};
+export const saveInterviewController = async (req: Request, res: Response) => {
+  try {
+    const payload = req.query as unknown as { interviewId: string };
 
-        if(!payload.interviewId){
-            res.status(code.BadRequest).json({msg:"Interview id is missing."});
-            return;
-        }
-        // 1. get user id
-        const userId = req.userId as string;
-
-        // 2. get interview id
-        const interviewId = payload.interviewId as string;
-
-        // 3. check if interview id exists or not
-        const doesInterviewExists = await prisma.interview.findFirst({
-            where:{
-                id:interviewId
-            }
-        })
-
-        // 4. if the record does not exists
-        if(!doesInterviewExists){
-            res.status(code.BadRequest).json({msg:"Invalid interview id"});
-            return;
-        }
-
-        // 5. if interview exists - save it (if not saved) delete it(if saved)
-        const isInterviewSaved = await prisma.savedInterview.findFirst({
-            where:{
-                userId,
-                interviewId,
-            }
-        })
-
-        // 6. If it is saved - then delete it
-        if(isInterviewSaved){
-            await prisma.savedInterview.delete({
-                where: {
-                    userId_interviewId: {
-                        userId,
-                        interviewId
-                    }
-                }
-            })
-        }
-
-        // 7. if it is not saved then save it
-        await prisma.savedInterview.create({
-            data:{
-                userId,
-                interviewId
-            }
-        });
-
-        res.status(code.SuccessNoContent).send();
-        return;
+    if (!payload.interviewId) {
+      res.status(code.BadRequest).json({ msg: "Interview id is missing." });
+      return;
     }
-    catch(err){
-        console.log("@saveInterview : \n",err);
-        res.status(code.ServerError).json({msg:"Internal Server Error"});
-        return;
+    // 1. get user id
+    const userId = req.userId as string;
+
+    // 2. get interview id
+    const interviewId = payload.interviewId as string;
+
+    // 3. check if interview id exists or not
+    const doesInterviewExists = await prisma.interview.findFirst({
+      where: {
+        id: interviewId,
+      },
+    });
+
+    // 4. if the record does not exists
+    if (!doesInterviewExists) {
+      res.status(code.BadRequest).json({ msg: "Invalid interview id" });
+      return;
     }
-}
+
+    // 5. if interview exists - save it (if not saved) delete it(if saved)
+    const isInterviewSaved = await prisma.savedInterview.findFirst({
+      where: {
+        userId,
+        interviewId,
+      },
+    });
+    
+    // 6. If it is saved - then delete it
+    if (isInterviewSaved) {
+      await prisma.savedInterview.delete({
+        where: {
+          id: isInterviewSaved.id,
+        },
+      });
+    } else {
+      // 7. if it is not saved then save it
+      await prisma.savedInterview.create({
+        data: {
+          userId,
+          interviewId,
+        },
+      });
+    }
+
+    res.status(code.SuccessNoContent).send();
+    return;
+  } catch (err) {
+    console.log("@saveInterview : \n", err);
+    res.status(code.ServerError).json({ msg: "Internal Server Error" });
+    return;
+  }
+};
