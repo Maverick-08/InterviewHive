795dcdc679816be1084c58d5bbad158b8ddd016f
Vinay <vinayojha1976@gmail.com>
2025-06-28 05:48:30 +0530

    faq modified

--- client/src/components/common/Faq1.tsx
+++ client/src/components/common/Faq1.tsx
@@ -16,49 +16,66 @@ interface Faq1Props {
 }
 
 const Faq1 = ({
-  heading = "Frequently asked questions",
+  heading = "Got Questions? We got you...",
   items = [
     {
-      question: "What is a Interview Hive?",
+      question: "What is Interview Hive?",
       answer:
-        "A FAQ is a list of frequently asked questions and answers on a particular topic.",
+        "Interview Hive is a student-driven platform where college students can share and explore real interview experiences to prepare for campus placements.",
     },
     {
-      question: "What is the purpose of a FAQ?",
+      question: "Who can contribute to Interview Hive?",
       answer:
-        "The purpose of a FAQ is to provide answers to common questions and help users find the information they need quickly and easily.",
+        "Final-year students who have attended placement interviews can contribute by sharing detailed accounts of their interview experiences.",
     },
     {
-      question: "How do I create a FAQ?",
+      question: "How does Interview Hive help juniors?",
       answer:
-        "To create a FAQ, you need to compile a list of common questions and answers on a particular topic and organize them in a clear and easy-to-navigate format.",
+        "Juniors can read shared experiences to understand what to expect in interviews-including questions asked, company-specific rounds,and preparation stratergies.",
     },
     {
-      question: "What are the benefits of a FAQ?",
+      question: "Can I save an Interview experience for later?",
       answer:
-        "The benefits of a FAQ include providing quick and easy access to information, reducing the number of support requests, and improving the overall user experience.",
+        "Yes! You can bookmark any experirence so it's easy to find and review during your placement preparation",
     },
+    {
+      question: "Is there a way to get help while using the platform?",
+      answer:
+        "Definitely. IH include a built-in chatbot assistant that helps you explore resources, find relevant interviews, and suggest preparation materials like mock questions and coding problems",
+    }
   ],
 }: Faq1Props) => {
   return (
-    <section className="mt-52 p-2 container max-w-4xl font-mono  border-white/15 rounded-md select-none">
-      <p className="py-2 text-center text-xs text-white/65">COMMON QUESTIONS</p>
-      <div className=" w-full">
-        <h1 className="mb-4 text-3xl font-semibold md:mb-11 md:text-4xl text-white/80 text-center">
-          {heading}
-        </h1>
-        <Accordion type="single" collapsible className=" ">
+    <section className="mt-20 sm:mt-52 p-4 container max-w-6xl font-mono  border-white/15 rounded-md select-none">
+      <div className="flex flex-col md:flex-row justify-between gap-8">
+        <div className="flex flex-col col-span-2">
+          <p className="py-2 text-start text-xs text-white/65">
+            COMMON QUESTIONS
+          </p>
+          <h1 className="mb-4 text-3xl font-semibold md:mb-11 md:text-4xl text-white/80 text-start max-w-xs text-balance">
+            {heading}
+          </h1>
+        </div>
+
+        <div>
+          <Accordion type="single" collapsible className="">
           {items.map((item, index) => (
-            <AccordionItem key={index} value={`item-${index}`} className="border-b-white/15">
-              <AccordionTrigger className="font-semibold hover:no-underline text-lg text-white">
+            <AccordionItem
+              key={index}
+              value={`item-${index}`}
+              className="border-b-white/15"
+            >
+              <AccordionTrigger className="sm:font-semibold hover:no-underline text-lg sm:text-xl text-white
+               hover:text-blue-400 transition-all duration-300 delay-100">
                 {item.question}
               </AccordionTrigger>
-              <AccordionContent className="text-muted-foreground">
+              <AccordionContent className="max-w-lg text-sm sm:text-md transiion-all text-white/70">
                 {item.answer}
               </AccordionContent>
             </AccordionItem>
           ))}
         </Accordion>
+        </div>
       </div>
     </section>
   );
