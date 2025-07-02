ffbd81bbeae9f2836287aab9b2309af15cc50da5
Vivek <ojhavivek24@gmail.com>
2025-07-02 23:10:22 +0530

    cleared app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "sonner";
 // import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Auth0Provider } from "@auth0/auth0-react";
-const clientId = import.meta.env.VITE_OAUTH_CLIENT_ID;
-const domain = import.meta.env.VITE_OAUTH_DOMAIN;
-const redirectUri = import.meta.env.VITE_OAUTH_REDIRECT_URI
+// import { Auth0Provider } from "@auth0/auth0-react";
+// const clientId = import.meta.env.VITE_OAUTH_CLIENT_ID;
+// const domain = import.meta.env.VITE_OAUTH_DOMAIN;
+// const redirectUri = import.meta.env.VITE_OAUTH_REDIRECT_URI
 
 const App = () => {
   return (
     <div>
-      <Auth0Provider
+      <Toaster richColors className="font-mono" />
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      {/* <Auth0Provider
         domain={domain}
         clientId={clientId}
         authorizationParams={{
@@ -21,7 +25,7 @@ const App = () => {
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
-      </Auth0Provider>
+      </Auth0Provider> */}
     </div>
   );
 };
