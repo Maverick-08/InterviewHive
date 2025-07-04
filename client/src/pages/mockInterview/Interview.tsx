4a04a706f195639ea9e1d91787a52bc84963cf85
Vivek <ojhavivek24@gmail.com>
2025-07-04 13:01:38 +0530

    added logic in record interview

--- client/src/pages/mockInterview/Interview.tsx
+++ client/src/pages/mockInterview/Interview.tsx
@@ -9,15 +9,16 @@ const Interview = () => {
   const toggleSidebar = useSidebarStore((state) => state.toggleSidebar);
   const [endInterview, setEndInterview] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
-
-  const { startRecording, stopRecording, mediaBlobUrl } =
-    useReactMediaRecorder({ video: true, audio: true, screen: true });
-
-  const videoConstraints = {
-    width: 600,
-    height: 330,
-    facingMode: "user",
-  };
+  const questions = [
+    "Explain Normalization",
+    "Explain the difference bewtween join and subqueries.",
+    "Explain logical schema",
+    "Explain SQL vs No Sql",
+  ];
+  const [counter, setCounter] = useState<number>(-1);
+  const { startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder(
+    { video: true, audio: true, screen: true }
+  );
 
   // Download
   useEffect(() => {
@@ -73,24 +74,70 @@ const Interview = () => {
       {/* Webcam & Questions  */}
       <div className="flex justify-between">
         {/* Recording Status & Webcam  */}
-        <div className="w-[60%] h-[50vh] p-4 flex flex-col gap-2 bg-[#333333] rounded-lg">
+        <div className="w-[60%] h-[50vh] p-4 flex flex-col gap-2 border border-white/15 bg-[#171717] rounded-lg">
           {/* Recording Status  */}
           <div className="flex items-center gap-2">
             <div className="relative">
               <div className=" h-2 w-2 rounded-full bg-red-600"></div>
-              <div className="absolute inset-0 h-2 w-2 rounded-full bg-red-600 animate-ping"></div>
+              <div
+                className={`absolute inset-0 h-2 w-2 rounded-full bg-red-600 ${
+                  isRecording ? "animate-ping" : ""
+                }`}
+              ></div>
             </div>
             <p>{isRecording ? "Recording" : "Recording Stopped"}</p>
           </div>
 
           {/* Web Cam  */}
-          <div>
-            <Webcam mirrored={true} videoConstraints={videoConstraints} />
+          <div className="h-full w-full">
+            <Webcam
+              mirrored={true}
+              className="object-cover"
+              style={{ width: "100%", height: "90%" }}
+            />
           </div>
         </div>
 
-        {/* Questions  */}
-        <div className="w-[35%] h-[40vh] border"></div>
+        <div className="w-[35%] h-[40vh] flex flex-col gap-4">
+          {/* Questions  */}
+          <div className="p-2 flex flex-col items-center gap-4 bg-[#171717] border border-[#333333] rounded-sm">
+            <p className="text-lg">
+              {counter >= 0
+                ? questions.at(counter)
+                : "Click on Start Button to start the Interview"}
+            </p>
+            <WhiteButton
+              disabled={counter == -1 || (counter>=questions.length)}
+              onClick={() => {
+                if (counter+1 < questions.length) {
+                  setCounter(counter + 1);
+                }
+              }}
+              text="Next Question"
+              className={`${
+                counter >= 0 && !(counter>=questions.length) ? "" : "bg-white/40 hover:bg-white/40"
+              } text-md`}
+            />
+          </div>
+
+          {/* progress bar  */}
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center justify-between">
+              <span>Progress</span>
+              <span>
+                Question {counter < 0 ? 0: counter+1} of {questions.length}
+              </span>
+            </div>
+            <div>
+              <div className="w-full rounded-sm bg-neutral-500">
+                <div
+                  className="h-1 rounded-sm  bg-yellow-500"
+                  style={{ width: `${counter < 0 ? 0 :((counter+1) / questions.length) * 100}%` }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Control Buttons  */}
@@ -119,6 +166,7 @@ const Interview = () => {
             onClick={() => {
               startRecording();
               setIsRecording(true);
+              setCounter(0);
             }}
             className="bg-gradient-to-b from-green-400 to-green-500 hover:bg-green-600 hover:scale-110 transition-all ease-in duration-300 delay-100 text-white font-sans"
           />
